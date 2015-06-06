3f18172cc2d910ba9a4bf3ae3002791029cd3c02
Aaron Krause <aaronjkrause@gmail.com>
2015-06-05 19:21:07 -0700

    basic logic and layout in place

--- app/js/controllers/board-controller.js
+++ app/js/controllers/board-controller.js
@@ -2,14 +2,29 @@
 
 module.exports = function(app) {
   app.controller('boardController', ['$scope', function($scope){
-    $scope.test = 'TEST';
     $scope.board = [
       ['A', 'B', 'C'],
       ['D', 'E', 'F'],
       ['G', 'H', 'I']
     ]
 
+    $scope.selectedWord = '';
 
+    $scope.addLetter = function(letter) {
+      $scope.selectedWord += letter;
+    }
+
+    $scope.clearWord = function() {
+      $scope.selectedWord = '';
+    }
+
+    $scope.backspace = function() {
+      $scope.selectedWord = $scope.selectedWord.slice(0, -1);
+    }
+
+    $scope.submitWord = function() {
+      $scope.selectedWord = '';
+    }
 
   }])
 }
