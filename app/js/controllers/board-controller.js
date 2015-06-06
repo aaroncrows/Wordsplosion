7fc8bf88d621787250e7e33e677a2cc8ee4033f8
Aaron Krause <aaronjkrause@gmail.com>
2015-06-05 23:07:32 -0700

    added logic for client graphing

--- app/js/controllers/board-controller.js
+++ app/js/controllers/board-controller.js
@@ -1,20 +1,53 @@
 'use strict';
+var ActiveTree = require('../../lib/graph');
+
+function LetterObject(location) {
+  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var rand = Math.floor(Math.random() * 26);
+
+  this.letter = alpha[rand];
+  this.location = location;
+}
+
+var board = (function() {
+  var board = [];
+  var row;
+  var rand;
+
+  for (var i = 0; i < 4; i++) {
+    row = [];
+    for (var j = 0;  j < 4; j++) {
+      
+      row.push(new LetterObject([i, j]));
+    };
+    board.push(row)
+  };
+  return board;
+})();
+
 
 module.exports = function(app) {
   app.controller('boardController', ['$scope', function($scope){
-    $scope.board = [
-      ['A', 'B', 'C'],
-      ['D', 'E', 'F'],
-      ['G', 'H', 'I']
-    ]
+    $scope.board = board;
+    $scope.tree = new ActiveTree(board);
+
 
     $scope.selectedWord = '';
+    $scope.canSubmit = true;
+    $scope.score;
 
     $scope.wordList = [];
     $scope.overflowList = [];
 
     $scope.addLetter = function(letter) {
-      $scope.selectedWord += letter;
+      $scope.selectedWord += letter.letter;
+      $scope.tree.activateNode(letter.location);
+      console.log($scope.isActive(letter));
+    }
+
+    $scope.isActive = function(letter) {
+      var loc = letter.location;
+      return $scope.tree.map[loc[0]][loc[1]];
     }
 
     $scope.clearWord = function() {
@@ -23,17 +56,30 @@ module.exports = function(app) {
 
     $scope.backspace = function() {
       $scope.selectedWord = $scope.selectedWord.slice(0, -1);
+      console.log($scope.canSubmit);
     }
 
     $scope.submitWord = function(word) {
-      var list = $scope.wordList.length <= 7 ? $scope.wordList : $scope.overflowList;
-      list.push($scope.selectedWord);
-      $scope.selectedWord = '';
+      if ($scope.selectedWord.length > 2) {
+        var list = $scope.wordList.length <= 7 ? $scope.wordList : $scope.overflowList;
+        list.push($scope.selectedWord);
+        $scope.selectedWord = '';
+      }
     }
 
     $scope.scoreBoard = function() {
+      var wordList = $scope.wordList.concat($scope.overflowList);
       $scope.wordList = [];
       $scope.overflowList = [];
+      $scope.score = 0;
+
+      for (var i = 0; i < wordList.length; i++) {
+        if(wordList[i].length < 8) {
+          $scope.score += wordList[i].length - 2;
+        } else {
+          $scope.score += 11;
+        }
+      };
     }
 
   }])
---
+++ app/lib/graph.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var BoardActiveTree = function(board) {
+  this.map = {};
+  for (var i = 0; i < board.length; i++) {
+    this.map[i] = {};
+    for (var j = 0; j < board[i].length; j++) {
+      this.map[i][j] = false;
+    }
+  }
+}
+
+BoardActiveTree.prototype.activateNode = function(location) {
+  var modifiers = [[1, 0], [0,1], [1, 1], [-1, 0], [0, -1],
+    [-1, -1], [-1, 1], [1, -1]];
+  var x;
+  var y;
+
+  for (var i = 0; i < modifiers.length; i++) {
+    x = location[0] + modifiers[i][0];
+    y = location[1] + modifiers[i][1];
+
+    if (this.map.hasOwnProperty(x) && this.map[x].hasOwnProperty(y)) {
+      this.map[x][y] = true;
+    }
+  };
+  this.map[location[0]][location[1]] = true;
+}
+
+module.exports = BoardActiveTree;
