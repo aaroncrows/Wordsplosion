42e0381604401bf24c16133bbd455db0d88bfe7d
Aaron Krause <aaronjkrause@gmail.com>
2017-05-11 18:49:25 -0700

    directive refactor

--- app/js/directives/gameboard/gameboard.js
+++ app/js/directives/gameboard/gameboard.js
@@ -3,18 +3,28 @@ var template = require('./gameboard.html');
 function GameBoard() {
   return {
     scope: {
-      board: '=',
       addLetter: '='
     },
-    controller: function(BoardService) {
-      this.isActive = BoardService.isActive;
-      this.isChosen = BoardService.isChosen;
-      // this.board = BoardService.getBoard();
-    },
+    controller: ctrl,
     bindToController: true,
-    controllerAs: 'boardCtrl',
+    controllerAs: 'ctrl',
     template: template
   };
 }
+
+function ctrl(BoardService, $scope) {
+  var vm = this;
+
+  vm.isActive = BoardService.isActive;
+  vm.isChosen = BoardService.isChosen;
+
+  $scope.$watch(function() {
+    return BoardService.getBoard();
+  }, function() {
+    vm.board = BoardService.getBoard();
+  });
+}
+
+ctrl.$inject = ['BoardService', '$scope'];
 
 module.exports = GameBoard;
--- app/js/directives/wordlist/wordlist.js
+++ app/js/directives/wordlist/wordlist.js
@@ -3,19 +3,23 @@ var template = require('./wordlist.html');
 function WordList() {
   return {
     restrict: 'E',
-    controller: function(GameService, $scope) {
-      var ctrl = this;
-      $scope.$watch(function() {
-        return GameService.getWordList()
-      }, function() {
-        ctrl.words = GameService.getWordList();
-      }, true);
-
-      this.words = GameService.getWordList();
-    },
+    controller: ctrl,
     controllerAs: 'ctrl',
     template
   };
 }
+
+function ctrl(GameService, $scope) {
+  var ctrl = this;
+  $scope.$watch(function() {
+    return GameService.getWordList()
+  }, function() {
+    ctrl.words = GameService.getWordList();
+  }, true);
+
+  this.words = GameService.getWordList();
+}
+
+ctrl.$inject = ['GameService', '$scope'];
 
 module.exports = WordList;
