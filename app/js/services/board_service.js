245fe801d960fad790b7b96c17b296eeb26f8122
Aaron Krause <aaronjkrause@gmail.com>
2017-05-15 14:32:48 -0700

    refactor board_service for better testing

--- app/js/services/board_service.js
+++ app/js/services/board_service.js
@@ -4,88 +4,50 @@ function BoardService($http, gameService) {
     [0, -1], [-1, -1], [-1, 1], [1, -1]
   ];
 
-  var service = {};
+  var svc = {};
 
-  var selected = {};
-  var boardHeight;
-  var boardWidth;
-  var lastPicked;
-  var board;
-  var activeSquares;
-
-  service.isChosen = function(letter) {
-    var loc = letter.location;
-    return selected[loc[0]] && selected[loc[0]][loc[1]];
-  };
-
-  service.initializeBoard = function(height, width) {
-    activeSquares = {};
-    boardHeight = height;
-    boardWidth = width;
+  svc.initializeBoard = function(height, width) {
+    svc.activeSquares = {};
+    svc.boardHeight = height;
+    svc.boardWidth = width;
 
     for (var i = 0; i < height; i++) {
-      activeSquares[i] = {};
+      svc.activeSquares[i] = {};
       for (var j = 0; j < width; j++) {
-        activeSquares[i][j] = false;
+        svc.activeSquares[i][j] = false;
       }
     }
-
-    return activeSquares;
   };
 
-  service.newGame = function() {
-    return $http.get('/new-game').then(function(data) {
-      data = data.data;
-      board = data.board;
+  svc.newGame = function() {
+    return $http.get('/new-game').then(function(response) {
+      var data = response.data;
+      svc.board = data.board;
       gameService.resetGame();
-      service.initializeBoard(data.board.length, data.board[0].length);
+      svc.initializeBoard(data.board.length, data.board[0].length);
 
       return data;
     });
   };
 
-  service.getBoard = function() {
-    return board;
-  };
-
-  service.isActive = function(letter) {
+  svc.addLetter = function(letter) {
     var loc = letter.location;
-    return activeSquares[loc[0]] && activeSquares[loc[0]][loc[1]];
-  };
+    var isAdjacent = svc.lastPicked ? svc.isActive(letter) : true;
+    var chosen = svc.isChosen(letter);
 
-  service.addLetter = function(letter) {
-    var loc = letter.location;
-    var isAdjacent = lastPicked ? service.isActive(letter) : true;
-    var chosen = service.isChosen(letter);
     if (chosen || !isAdjacent) return false;
 
-    if (!selected[loc[0]]) selected[loc[0]] = {};
-    selected[loc[0]][loc[1]] = true;
-    lastPicked = loc;
+    if (!svc.selected[loc[0]]) svc.selected[loc[0]] = {};
+    svc.selected[loc[0]][loc[1]] = true;
+    svc.lastPicked = loc;
 
-    service.deactivateAll();
-    service.activateNode(letter.location);
+    svc.deactivateAll();
+    svc.activateNode(letter.location);
 
     return true;
   };
 
-  service.resetBoard = function() {
-    lastPicked = null;
-    selected = {};
-
-    service.deactivateAll();
-  };
-
-  service.deactivateAll = function() {
-    for (var i = 0; i < boardHeight; i++) {
-      activeSquares[i] = {};
-      for (var j = 0; j < boardWidth; j++) {
-        activeSquares[i][j] = false;
-      }
-    }
-  };
-
-  service.activateNode = function(location) {
+  svc.activateNode = function(location) {
     var x;
     var y;
 
@@ -93,15 +55,45 @@ function BoardService($http, gameService) {
       x = location[0] + ADJACENT_COORDS[i][0];
       y = location[1] + ADJACENT_COORDS[i][1];
 
-      if (activeSquares.hasOwnProperty(x) && activeSquares[x].hasOwnProperty(y)) {
-        activeSquares[x][y] = true;
+      if (svc.activeSquares.hasOwnProperty(x) && svc.activeSquares[x].hasOwnProperty(y)) {
+        svc.activeSquares[x][y] = true;
       }
     };
 
-    activeSquares[location[0]][location[1]] = true;
+    svc.activeSquares[location[0]][location[1]] = true;
   };
 
-  return service;
+  svc.getBoard = function() {
+    return svc.board;
+  };
+
+  svc.isChosen = function(letter) {
+    var loc = letter.location;
+    return svc.selected[loc[0]] && svc.selected[loc[0]][loc[1]];
+  };
+
+  svc.isActive = function(letter) {
+    var loc = letter.location;
+    return svc.activeSquares[loc[0]] && svc.activeSquares[loc[0]][loc[1]];
+  };
+
+  svc.deactivateAll = function() {
+    for (var i = 0; i < svc.boardHeight; i++) {
+      svc.activeSquares[i] = {};
+      for (var j = 0; j < svc.boardWidth; j++) {
+        svc.activeSquares[i][j] = false;
+      }
+    }
+  };
+
+  svc.resetBoard = function() {
+    svc.lastPicked = null;
+    svc.selected = {};
+
+    svc.deactivateAll();
+  };
+
+  return svc;
 }
 
 BoardService.$inject = ['$http', 'GameService'];
